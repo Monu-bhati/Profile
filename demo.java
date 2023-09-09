1687bc57b2e5f740f10367aee9afd2718cefe646
Monu Bhati <113228628+Monu-bhati@users.noreply.github.com>
2023-09-09 09:52:02 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,9 @@
+class demo{
+    public static void main(String [] args){
+
+        int num = 4;
+        int num2 = 5;
+        System.out.println("Hello World");
+        System.out.println(num+num2);
+    }
+}
